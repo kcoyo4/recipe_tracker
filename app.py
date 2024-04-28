60e1346ae54ec59ae519ecc0b4ac26b494a03ba9
kcoyo4 <kyoca9@gmail.com>
2024-04-28 07:56:29 -0700

    commit

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import tkinter as tk
 import customtkinter as ctk
 
 
-
 from controllers.IngredientPageController import * 
 from controllers.DBUtil import *
 from controllers.AddRecipeController import * 
